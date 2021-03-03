49b898856a34fa9109271e8580bd2cac10774186
Darian Hernandez <darianhernandezcs@gmail.com>
2021-03-03 12:15:02 -0500

    Added simple timer
    Added a timer with a start/stop button, along with a spinner that will hold profiles that the user will create

--- app/src/main/java/com/example/mobilegroupproject/MainActivity.java
+++ app/src/main/java/com/example/mobilegroupproject/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.mobilegroupproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
 
+    static Chronometer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timer = (Chronometer) findViewById(R.id.timer);
+        Button stop = (Button) findViewById(R.id.stopTimer);
+        stop.setVisibility(View.INVISIBLE);
+        stop.setClickable(false);
+    }
+
+    public void startTimer(View view)
+    {
+        timer.setBase(SystemClock.elapsedRealtime());
+        timer.start();
+        Button stop = (Button) findViewById(R.id.stopTimer);
+        stop.setVisibility(View.VISIBLE);
+        stop.setClickable(true);
+        Button start = (Button) findViewById(R.id.startTimer);
+        start.setVisibility(View.INVISIBLE);
+        start.setClickable(false);
+    }
+
+    public void stopTimer(View view)
+    {
+        timer.setBase(SystemClock.elapsedRealtime());
+        timer.stop();
+        Button start = (Button) findViewById(R.id.startTimer);
+        start.setVisibility(View.VISIBLE);
+        start.setClickable(true);
+        Button stop = (Button) findViewById(R.id.stopTimer);
+        stop.setVisibility(View.INVISIBLE);
+        stop.setClickable(false);
     }
 }
