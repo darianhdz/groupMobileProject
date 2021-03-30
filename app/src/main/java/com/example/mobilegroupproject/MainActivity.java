e367867c32dd7e84f525312117602c1419148de6
Darian Hernandez <darianhernandezcs@gmail.com>
2021-03-30 16:06:46 -0400

    added a few things
    Changing text color for timer, custom background, and new toolbar

--- app/src/main/java/com/example/mobilegroupproject/MainActivity.java
+++ app/src/main/java/com/example/mobilegroupproject/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.mobilegroupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
         timer = (Chronometer) findViewById(R.id.timer);
         Button stop = (Button) findViewById(R.id.stopTimer);
         stop.setVisibility(View.INVISIBLE);
@@ -78,13 +94,13 @@ public class MainActivity extends AppCompatActivity {
                 mSelectionClause, mSelectionArgs);
     }
 
-    public void addEntries(View view)
+    public void addEntries()
     {
         MyDialogFragment f = new MyDialogFragment();
         f.show(getSupportFragmentManager(), "add");
     }
 
-    public void removeEntries(View view)
+    public void removeEntries()
     {
         MyDialogFragment f = new MyDialogFragment();
         f.show(getSupportFragmentManager(), "remove");
@@ -106,9 +122,64 @@ public class MainActivity extends AppCompatActivity {
         selection.setAdapter(adapter);
     }
 
-    public void toDataActivity(View view)
+    public void toDataActivity()
     {
         Intent intent = new Intent(getApplicationContext(), TimeAggregation.class);
         getApplicationContext().startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //from contextmenu example
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addActivity:
+                addEntries();
+                break;
+            case R.id.removeActivity:
+                removeEntries();
+                break;
+
+            case R.id.showTimeData:
+                toDataActivity();
+                break;
+
+            case R.id.changeBackground:
+                Intent pickPic = new Intent(Intent.ACTION_PICK);
+                pickPic.setType("image/*");
+                startActivityForResult(pickPic, 1);
+                break;
+
+            case R.id.changeTimerColor:
+                MyDialogFragment f = new MyDialogFragment();
+                f.show(getSupportFragmentManager(), "color");
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri); //https://stackoverflow.com/questions/38352148/get-image-from-the-gallery-and-show-in-imageview
+                //for help on picking images from a gallery
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                ImageView background = findViewById(R.id.backgroundView);
+                background.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mobilegroupproject/MyDialogFragment.java
+++ app/src/main/java/com/example/mobilegroupproject/MyDialogFragment.java
@@ -7,14 +7,21 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.fragment.app.DialogFragment;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MyDialogFragment extends DialogFragment {
     Cursor mCursor;
@@ -86,6 +93,44 @@ public class MyDialogFragment extends DialogFragment {
             }
             builder.setTitle("In the Activity " + spinner.getSelectedItem().toString() + " you have spent a total of ");
             builder.setMessage(Math.ceil(savedTime/60) + " minutes.");
+        }
+        else if(getTag().equals("color"))
+        {
+            builder.setTitle("Change Text Color of Timer");
+            builder.setMessage("Choose the color: ");
+            Spinner selection = new Spinner(getActivity());
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1);
+            List<String> stringsList = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.color_array)));
+            adapter.addAll(stringsList);
+            selection.setAdapter(adapter);
+            builder.setView(selection);
+            builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                   String selected = selection.getSelectedItem().toString();
+                    Chronometer timer = getActivity().findViewById(R.id.timer);
+                   switch(selected) {
+                       case "White":
+                           timer.setTextColor(Color.WHITE);
+                           break;
+                       case "Red":
+                           timer.setTextColor(Color.RED);
+                           break;
+                       case "Blue":
+                           timer.setTextColor(Color.BLUE);
+                           break;
+                       case "Green":
+                           timer.setTextColor(Color.GREEN);
+                           break;
+                       case "Black":
+                           timer.setTextColor(Color.BLACK);
+                           break;
+                       case "Grey":
+                           timer.setTextColor(Color.GRAY);
+                           break;
+                   }
+                }
+            });
         }
         return builder.create();
     }
